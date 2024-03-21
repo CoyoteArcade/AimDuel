e6673a40e9cb8de125d41bbca7dd3a2eb02dd527
servin415 <143432937+servin415@users.noreply.github.com>
2024-03-20 20:37:24 -0700

    Added bullet impact

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RayShooter : MonoBehaviour
 {
+    // Bullet hit affect prefab
+    public GameObject BulletHitPrefab;
+
     // Private variable that has reference to Camera
     private Camera cam;
 
@@ -20,7 +23,8 @@ public class RayShooter : MonoBehaviour
     }
 
     // OnGUI unity method to draw crosshairs onscreen
-    void OnGUI() {
+    void OnGUI()
+    {
         // font size
         int size = 12;
 
@@ -36,7 +40,8 @@ public class RayShooter : MonoBehaviour
     void Update()
     {
         // 0 for left button, 1 for right button, 2 for the middle button
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             // Use a Vector3 to store the location of the middle of the screen
             // Divide the width and height by 2 to get the midpoint; these become
             // pixelWidth and pixelHeight give you size of the screen
@@ -51,38 +56,35 @@ public class RayShooter : MonoBehaviour
             RaycastHit hit;
 
             // out parameter uses same reference
-            if (Physics.Raycast(ray, out hit)) {
+            if (Physics.Raycast(ray, out hit))
+            {
                 // Get reference to object that was hit
                 // Uses transform component's gameObject property
                 GameObject hitObject = hit.transform.gameObject;
-                
+
                 // Get reference to object's ReactiveTarget Script if it exists
                 ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
 
                 // If ray hits enemy, indicate an enemy was hit, otherwise place a sphere
-                if (target != null) {
+                if (target != null)
+                {
                     target.ReactToHit();
-                } else {
-                    StartCoroutine(SphereIndicator(hit.point));
                 }
-   
+                else
+                {
+                    // Bullet hit effect at hit point
+                    CreateBulletHitEffect(hit.point + hit.normal * 0.001f, hit.normal);
+                }
+
             }
         }
     }
 
-    // Coroutine, relates to IEnumerator
-    // This places a sphere at a set of coords, then removes the sphere after 1 sec
-    private IEnumerator SphereIndicator(Vector3 pos) {
-        // Create a new game object: sphere
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    private void CreateBulletHitEffect(Vector3 pos, Vector3 hitNormal)
+    {
+        // Instantiate the bullet hit effect at the hit point
+        GameObject bulletHitEffect = Instantiate(BulletHitPrefab, pos, Quaternion.LookRotation(hitNormal));
 
-        // Place at given position
-        sphere.transform.position = pos;
-
-        // Then wait 1 sec
-        // yield is where coroutine pauses
-        yield return new WaitForSeconds(1);
-
-        Destroy(sphere);
+        Destroy(bulletHitEffect, 1f);
     }
 }
