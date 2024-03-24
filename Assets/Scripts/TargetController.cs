d02ae22e6e681d01efe2b7bdb2150107393adb01
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2024-03-23 17:48:13 -0700

    Spawn targets based on dimension size (N x N)

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -7,34 +7,36 @@ using UnityEngine;
 public class TargetController : MonoBehaviour
 {
     [SerializeField] GameObject targetPrefab;
-    [SerializeField] int numTargets;
+    [SerializeField] int targetDimensions;
     [SerializeField] int numVisibleTargets;
     private GameObject target;
-
-    // Position of the first target
-    private Vector3 startPos;
+    private int numOfTargets;
 
     // Offset distance from the first target's position;
-    public const float offsetX = 2f;
+    public const float offsetX = 3f;
+    public const float offsetY = 3f;
     
     // Start is called before the first frame update
     void Start()
     {
-        numTargets = 3;
+        Vector3 startPos = new Vector3(0, 0, 0);
+        targetDimensions = 4;
+        numOfTargets = targetDimensions * targetDimensions;
         numVisibleTargets = 2;
 
         // Creates target instances with Target Plane as parent
-        for (int i = 0; i < numTargets; i++)
+        for (int i = 0; i < targetDimensions; i++)
         {
-            target = Instantiate(targetPrefab, this.transform) as GameObject;
-            if (i == 0) {
-                target.transform.localPosition = new Vector3(0, 0, 0);
-                startPos = target.transform.localPosition;
-            } else {
-                Debug.Log($"New Position: {startPos.x * i}");
-                target.transform.localPosition = new Vector3(offsetX * i + startPos.x, 0, 0);
+            for (int j = 0; j < targetDimensions; j++)
+            {
+                target = Instantiate(targetPrefab, this.transform) as GameObject;
+                if (i == 0 && j == 0) {
+                    target.transform.localPosition = new Vector3(0, 0, 0);
+                    startPos = target.transform.localPosition;
+                } else {
+                    target.transform.localPosition = new Vector3(offsetX * i + startPos.x, -(offsetY * j + startPos.y), 0);
+                }
             }
-
         }
     }
 
@@ -57,8 +59,8 @@ public class TargetController : MonoBehaviour
     void Update()
     {
         // Keep visible targets less than total amount of targets
-        if (numVisibleTargets >= numTargets) {
-            numVisibleTargets = numTargets - 1;
+        if (numVisibleTargets >= numOfTargets) {
+            numVisibleTargets = numOfTargets - 1;
         }
     }
 }
