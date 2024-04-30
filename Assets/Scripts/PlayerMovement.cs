ee2f6a84fcccbf0b02062ba150b25115541a1204
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2024-04-29 22:02:31 -0700

    Add player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,19 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D body;
+    CapsuleCollider2D collider;
     new SpriteRenderer renderer;
     Animator animator;
 
     [SerializeField] float moveSpeed = 2.0f;
+    [SerializeField] float jumpSpeed = 7.0f;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -33,10 +36,20 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnJump(InputValue value) {
+        bool isGrounded = collider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if(value.isPressed && isGrounded) {
+            body.velocity = Vector2.up * jumpSpeed;
+        }
+    }
+
     void Run() {
         bool playerMovesDiagonal = Mathf.Abs(moveInput.x) > Mathf.Epsilon && Mathf.Abs(moveInput.y) > Mathf.Epsilon;
 
         Vector2 playerVelocity;
+
+        // Keeps speed from slowing down when input is diagonal
         if (playerMovesDiagonal) {
             playerVelocity = new Vector2(Mathf.Sign(moveInput.x) * moveSpeed, body.velocity.y);
         } else {
