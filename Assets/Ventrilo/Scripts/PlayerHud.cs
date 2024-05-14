976a78e6b82743c4cfb8d82838fa3bc6b7db378e
Ravy Thach <10255290+rthach17@users.noreply.github.com>
2024-05-13 18:52:07 -0700

    Add dynamic HUD portrait

---
+++ Assets/Ventrilo/Scripts/PlayerHud.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHud : MonoBehaviour
+{
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] Sprite portrait;
+    [SerializeField] Sprite portraitHurt;
+    [SerializeField] Sprite portraitDead;
+    bool isHurt;
+    bool isDead;
+
+    Image portraitImage;
+
+    void Start() {
+        portraitImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        isHurt = playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Hurt");
+        isDead = playerAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Dead");
+
+        ChangePortrait();
+    }
+
+    void ChangePortrait() {
+        if (isDead) {
+            portraitImage.sprite = portraitDead;
+        } else if (isHurt) {
+            portraitImage.sprite = portraitHurt;
+        } else {
+            portraitImage.sprite = portrait;
+        }
+    }
+}
